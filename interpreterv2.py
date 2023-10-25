dabc8fefddac432cf8b6935d58a193cf45244c98
Jackson Steele <jacksonrobertsteele@gmail.com>
2023-10-25 16:54:42 -0700

    Statement first iteration

--- interpreterv2.py
+++ interpreterv2.py
@@ -1,9 +1,9 @@
 from intbase import InterpreterBase, ErrorType
 from brewparse import parse_program
 from element import Element
+from constants import *
 
 class Interpreter(InterpreterBase):
-    
     FUNCTIONS = "functions"
     VALUE = "val"
     NAME = "name"
@@ -13,44 +13,74 @@ class Interpreter(InterpreterBase):
     ASSIGNMENT = "="
     ADD = "+"
     SUBTRACT = "-"
+    MULTIPLY = "*"
+    DIVIDE = "/"
+    EQUALS = "=="
+    NOT_EQUALS = "!="
+    LESS_THAN = "<"
+    LESS_THAN_EQUALS = "<="
+    GREATER_THAN = ">"
+    GREATER_THAN_EQUALS = ">="
+    OR = "||"
+    AND = "&&"
+    BINARY_INT_OPERATORS = [ADD, SUBTRACT, MULTIPLY, DIVIDE]
+    COMPARISON_INT_OPERATORS = [EQUALS, NOT_EQUALS, LESS_THAN, LESS_THAN_EQUALS, GREATER_THAN, GREATER_THAN_EQUALS]
+    UNARY_INT_OPERATORS = [InterpreterBase.NEG_DEF]
+    BINARY_BOOL_OPERATORS = [OR, AND]
+    COMPARISON_BOOL_OPERATORS = [EQUALS, NOT_EQUALS]
+    UNARY_BOOL_OPERATORS = [InterpreterBase.NOT_DEF]
+    BINARY_STRING_OPERATORS = [ADD]
+    COMPARISON_STRING_OPERATORS = [EQUALS, NOT_EQUALS]
+    UNARY_STRING_OPERATORS = []
+    BINARY_OPERATORS = BINARY_INT_OPERATORS + BINARY_BOOL_OPERATORS + BINARY_STRING_OPERATORS
+    COMPARSION_OPERATORS = COMPARISON_INT_OPERATORS + COMPARISON_BOOL_OPERATORS + COMPARISON_STRING_OPERATORS
+    UNARY_OPERATORS = UNARY_INT_OPERATORS + UNARY_BOOL_OPERATORS + UNARY_STRING_OPERATORS
+    OPERATORS = BINARY_OPERATORS + UNARY_OPERATORS
     OPERAND_1 = "op1"
     OPERAND_2 = "op2"
     ARGS = "args"
-    STATEMENT_TYPES = [ASSIGNMENT, InterpreterBase.FCALL_DEF, InterpreterBase.IF_DEF, InterpreterBase.WHILE_DEF, InterpreterBase.RETURN_DEF]
-    EXPRESSION_TYPES = [InterpreterBase.FCALL_DEF, ADD, SUBTRACT]
-    VALUE_TYPES = [InterpreterBase.INT_DEF, InterpreterBase.STRING_DEF]
-    INPUT = "inputi"
+    INPUTI = "inputi"
+    INPUTS = "inputs"
     PRINT = "print"
-    PRELOADED_FUNCS = [PRINT, INPUT]
+    PRELOADED_FUNCS = [PRINT, INPUTI, INPUTS]
+    STATEMENT_TYPES = [ASSIGNMENT, InterpreterBase.FCALL_DEF, InterpreterBase.IF_DEF, InterpreterBase.WHILE_DEF, InterpreterBase.RETURN_DEF]
+    STATEMENT_TYPES += PRELOADED_FUNCS
+    EXPRESSION_TYPES = [InterpreterBase.FCALL_DEF] + OPERATORS
+    VALUE_TYPES = [InterpreterBase.INT_DEF, InterpreterBase.STRING_DEF, InterpreterBase.BOOL_DEF, InterpreterBase.NIL_DEF]
+    NIL_VAL = Element(InterpreterBase.NIL_DEF)
 
     def __init__(self, console_output=True, inp=None, trace_output=False):
         super().__init__(console_output, inp)
         self.function_map = {}
-        self.var_map = {}
         self.preloaded_funcs = set()
         self.trace_output = trace_output
     
-    def get_func_name(self, function):
-        return function.get(Interpreter.NAME)
+    def get_func_name(self, function: Element):
+        if function.elem_type != InterpreterBase.FUNC_DEF:
+            super().error(ErrorType.TYPE_ERROR, f"Tried to get name of node {str(function)}, which is not a function but a {function.elem_type}")
+            return None
+        return function.get(NAME)
     
     def load_func(self, function: Element) -> None:
         # Checking we were passed a function element
         if function.elem_type != InterpreterBase.FUNC_DEF:
             super().error(ErrorType.TYPE_ERROR, f"Tried to load node {str(function)}, which is not a function but a {function.elem_type}")
             return
-        
         name = self.get_func_name(function)
         if not name:
             super().error(ErrorType.FAULT_ERROR, f"No name associated with the function {str(function)}")
             return
-        num_args = len(function.get(Interpreter.ARGS))
+        num_args = len(function.get(ARGS))
         # For each function, we store it as a map between the function name and a map between the number of arguments and the function itself
         # This allows us to overload functions
         if name in self.function_map:
             # Overloading the function
-            self.function_map[name][num_args] = function
             if self.trace_output:
                 print(f"Function {name} already exists, overloading it")
+            if num_args in self.function_map[name]:
+                if self.trace_output:
+                    print(f"Function {name} with {num_args} arguments already exists!  Overwriting it")
+            self.function_map[name][num_args] = function
         
         self.function_map[name] = {num_args: function}
         if self.trace_output:
@@ -71,13 +101,15 @@ class Interpreter(InterpreterBase):
     # Function we run at the beginning to load any functions we hardcode in (e.g. print)
     def preload_funcs(self) -> None:
         # Constructing the print and inputi functions
-        print_statements = [] # No statements - currently preloaded funcs are hardcoded
-        input_statements = []
+        print_statements = [Element(elem_type=PRINT)]
+        inputi_statements = [Element(elem_type=INPUTI)]
+        inputs_statements = [Element(elem_type=INPUTS)]
 
-        print_func = Element(InterpreterBase.FUNC_DEF, name=Interpreter.PRINT, statement = print_statements)
-        input_func = Element(InterpreterBase.FUNC_DEF, name=Interpreter.INPUT, statement = input_statements)
+        print_func = Element(InterpreterBase.FUNC_DEF, name=PRINT, statement = print_statements)
+        inputi_func = Element(InterpreterBase.FUNC_DEF, name=INPUTI, statement = inputi_statements)
+        inputs_func = Element(InterpreterBase.FUNC_DEF, name=INPUTS, statement = inputs_statements)
         
-        funcs_to_load = [print_func, input_func]
+        funcs_to_load = [print_func, inputi_func, inputs_func]
 
         for func in funcs_to_load:
             self.preload_func(func)
@@ -94,25 +126,6 @@ class Interpreter(InterpreterBase):
 
     def is_preloaded(self, function_name):
         return function_name in self.preloaded_funcs
-    
-    def run_preloaded_func(self, function, args):
-        if self.trace_output:
-            print(f"Running preloaded function {str(function)} with args {str(args)}")
-        match self.get_func_name(function):
-                case Interpreter.PRINT:
-                    super().output("".join([str(arg.get(Interpreter.VALUE)) for arg in args]))
-                case Interpreter.INPUT:
-                    if len(args) > 1:
-                        return super().error(ErrorType.NAME_ERROR, "Inputi function takes at most one argument")
-                    if len(args) == 1:
-                        super().output(str(args[0].get(Interpreter.VALUE)))
-                    input_val = super().get_input()
-                    if input_val.isnumeric():
-                        return Element(InterpreterBase.INT_DEF, val=int(input_val))
-                    else:
-                        super.error(ErrorType.TYPE_ERROR, f"Inputi function expected int, got {input_val}")
-                case _:
-                    super().error(ErrorType.TYPE_ERROR, f"Preloaded function {function.get(Interpreter.NAME)} not implemented")
             
     
     def run(self, program: str) -> None:
@@ -127,97 +140,22 @@ class Interpreter(InterpreterBase):
         self.var_map = {}
         self.preload_funcs()
         # Loading functions
-        for function in root_node.get(Interpreter.FUNCTIONS):
+        for function in root_node.get(FUNCTIONS):
             self.load_func(function)
-        if Interpreter.MAIN_FUNC_NAME not in self.function_map:
-            super().error(ErrorType.NAME_ERROR, f"Main function {Interpreter.MAIN_FUNC_NAME} not found")
+        if MAIN_FUNC_NAME not in self.function_map:
+            super().error(ErrorType.NAME_ERROR, f"Main function {MAIN_FUNC_NAME} not found")
         if self.trace_output:
             print("Running main")
         # Now we can evaluate the program - assume it has no arguments passed
-        self.run_function(Interpreter.MAIN_FUNC_NAME, [])
-            
-
-    def run_function(self, function_name, args):
-        if self.trace_output:
-            print(f"Running {function_name} with args {str(args)}")
-        num_args = len(args)
-        function = self.get_func(function_name, num_args)
-        # Evaluating the arguments before running the function
-        eval_args = []
-        for arg in args:
-            eval_args.append(self.evaluate_node(arg))
-        if self.trace_output:
-            print(f"Evaluated args: {[str(arg) for arg in eval_args]}")
-        
-        # Preloaded functions are treated differently than user-defined functions
-        if self.is_preloaded(function_name):
-            return self.run_preloaded_func(function, eval_args)
-
-        # Executing all statements in the function
-        for statement in function.get(Interpreter.STATEMENT):
-            # For the future - attempt at implementing return statements.  Not necessary for project 1
-            """
-            if statement.elem_type == InterpreterBase.RETURN_DEF:
-                return self.evaluate_node(statement.get(Interpreter.EXPRESSION))
-            """
-            self.run_statement(statement)
-        if self.trace_output:
-            print(f"Finished running {function_name}")
-    
+        self.run_function(MAIN_FUNC_NAME, [])
 
     # We run the statement by checking its type and executing the appropriate code
     def run_statement(self, statement) -> None:
         match statement.elem_type:
-            case Interpreter.ASSIGNMENT:
+            case str(ASSIGNMENT):
                 self.run_assignment(statement)
             case InterpreterBase.FCALL_DEF:
-                self.run_function(statement.get(Interpreter.NAME), statement.get(Interpreter.ARGS))
+                self.run_function(statement.get(NAME), statement.get(ARGS))
             case _:
                 super().error(ErrorType.TYPE_ERROR, "Statement type not recognized")
                 return
-    
-    # Assignment makes use of our var_map to store the variable name and its value, which is the evaluation of the expression
-    def run_assignment(self, statement) -> None:
-        if self.trace_output:
-            print(f"Running assignment {statement}")
-        self.var_map[statement.get(Interpreter.NAME)] = self.evaluate_node(statement.get(Interpreter.EXPRESSION))
-        
-    
-    # Evaluates Value, Variable, and Expression nodes
-    def evaluate_node(self, node) -> Element:
-        type = node.elem_type
-        # Most value types just directly return their values, no shenanigans necessary
-        if type in Interpreter.VALUE_TYPES:
-            return node
-
-        # Variables are just looked up in the var_map - they are executed upon assignment, not lazily upon use, so we don't need to worry about them much
-        elif type == InterpreterBase.VAR_DEF:
-            if (node.get(Interpreter.NAME) not in self.var_map):
-                super().error(ErrorType.NAME_ERROR, f"Variable {node.get(Interpreter.NAME)} not found")
-            return self.var_map[node.get(Interpreter.NAME)]
-        
-        # Expressions are evaluated recursively - if it's a function, we'll run the function, but otherwise we just evaluate the operands recursively until we get returned values which are then used
-        elif type in Interpreter.EXPRESSION_TYPES:
-            match type:
-                case InterpreterBase.FCALL_DEF:
-                    return self.run_function(node.get(Interpreter.NAME), node.get(Interpreter.ARGS))
-                case Interpreter.ADD:
-                    op1 = self.evaluate_node(node.get(Interpreter.OPERAND_1))
-                    op2 = self.evaluate_node(node.get(Interpreter.OPERAND_2))
-                    if op1.elem_type != InterpreterBase.INT_DEF or op2.elem_type != InterpreterBase.INT_DEF:
-                        super().error(ErrorType.TYPE_ERROR, "Invalid type for addition - expected int, got string")
-                        # return str(self.evaluate_node(node.get(Interpreter.OPERAND_1)).get(Interpreter.VALUE)) + str(self.evaluate_node(node.get(Interpreter.OPERAND_2)).get(Interpreter.VALUE))
-                    sum_vals = op1.get(Interpreter.VALUE) + op2.get(Interpreter.VALUE)
-                    return Element(InterpreterBase.INT_DEF, val=sum_vals)
-                case Interpreter.SUBTRACT:
-                    op1 = self.evaluate_node(node.get(Interpreter.OPERAND_1))
-                    op2 = self.evaluate_node(node.get(Interpreter.OPERAND_2))
-                    if op1.elem_type != InterpreterBase.INT_DEF or op2.elem_type != InterpreterBase.INT_DEF:
-                        super().error(ErrorType.TYPE_ERROR, "Invalid type for addition - expected int, got string")
-                        # return str(self.evaluate_node(node.get(Interpreter.OPERAND_1)).get(Interpreter.VALUE)) + str(self.evaluate_node(node.get(Interpreter.OPERAND_2)).get(Interpreter.VALUE))
-                    diff_vals = op1.get(Interpreter.VALUE) - op2.get(Interpreter.VALUE)
-                    return Element(InterpreterBase.INT_DEF, val=diff_vals)
-                case _:
-                    super().error(ErrorType.TYPE_ERROR, f"Valid expression type {type} but executing the expression isn't yet implemented")
-        else:
-            super().error(ErrorType.TYPE_ERROR, f"{type} cannot be evaluated")
--- statement.py
+++ statement.py
@@ -1,22 +1,212 @@
 from intbase import ErrorType
 from element import Element
-from interpreterv2 import InterpreterV2
-
+from intbase import InterpreterBase
+from constants import *
 class Statement:
-    def __init__(self, interpreter, parent_statement, type, trace_output=False):
+    def __init__(self, interpreter, parent_statement, statement_node: Element):
+        if type(interpreter) != InterpreterBase:
+            raise Exception("Interpreter must be of type InterpreterBase")
         self.parent_statement = parent_statement
-        self.type = type
+        self.statement_node = statement_node
+        if not statement_node.elem_type in interpreter.statement_types:
+            interpreter.super().error(ErrorType.TYPE_ERROR, f"Statement type {statement_node.elem_type} not recognized")
+            return
+        self.type = statement_node.elem_type
+        self.trace_output = interpreter.trace_output
         if parent_statement:
-            if trace_output:
+            if self.trace_output:
                 print("Creating new statement under parent statement")
-            self.root_var_map = parent_statement.var_map
         else:
-            if trace_output:
+            if self.trace_output:
                 print("Creating new root statement")
-            self.root_var_map = {}
         
         # Because we can't declare functions within the statement of other functions, this is fine
-        self.function_map = function_map
+        self.function_map = interpreter.function_map
+        self.var_map = {}
 
-        self.local_var_map = {}
-        self.trace_output = trace_output
+    # We recursively check if the variable exists in the current statement or any parent statements
+    # We stop when we reach the invoking function
+    def get_var_scope(self, name: str):
+        if name in self.var_map:
+            return self
+        if self.type == InterpreterBase.FUNC_DEF:
+            return None
+        # Recursively get the scope
+        if not self.parent_statement:
+            if self.trace_output:
+                print(f"All non-function statements should have a parent statement, but this one doesn't!  Very interesting...")
+            return None
+        
+        return self.parent_statement.get_var_scope(name)
+
+    def get_var(self, name: str) -> Element:
+        scope = self.get_var_scope(name)
+        if not scope:
+            self.interpreter.super().error(ErrorType.NAME_ERROR, f"No such variable as {name}")
+            return NIL_VAL
+        return scope.var_map[name]
+
+    def set_var(self, name: str, value: Element) -> None:
+        scope = self.get_var_scope(name)
+        if not scope:
+            self.var_map[name] = value
+        else:
+            scope.var_map[name] = value
+    
+    def evaluate_unary_operation(self, expression: Element) -> Element:
+        val = self.evaluate_expression(expression.get(OPERAND_1))
+        type = val.elem_type
+        if type == InterpreterBase.INT_DEF and not (type in UNARY_INT_OPERATORS):
+            self.interpreter.super().error(ErrorType.TYPE_ERROR, f"Got an int when expected another datatype")
+            return NIL_VAL
+        if type == InterpreterBase.BOOL_DEF and not (type in UNARY_BOOL_OPERATORS):
+            self.interpreter.super().error(ErrorType.TYPE_ERROR, f"Got a bool when expected another datatype")
+            return NIL_VAL
+        if type == InterpreterBase.STRING_DEF and not (type in UNARY_STRING_OPERATORS):
+            self.interpreter.super().error(ErrorType.TYPE_ERROR, f"Got a string when expected another datatype")
+            return NIL_VAL
+        match expression.elem_type:
+            case InterpreterBase.NEG_DEF:
+                return Element(InterpreterBase.INT_DEF, value=-val.get(VALUE))
+            case InterpreterBase.NOT_DEF:
+                return Element(InterpreterBase.BOOL_DEF, value=not val.get(VALUE))
+    
+    def evaluate_binary_operation(self, expression: Element) -> Element:
+        val1 = self.evaluate_expression(expression.get(OPERAND_1))
+        val2 = self.evaluate_expression(expression.get(OPERAND_2))
+        type1 = val1.elem_type
+        type2 = val2.elem_type
+        if type1 != type2:
+            self.interpreter.super().error(ErrorType.TYPE_ERROR, f"Got {type1} and {type2} when expected same datatype")
+            return NIL_VAL
+        if type1 == InterpreterBase.INT_DEF and not (type1 in BINARY_INT_OPERATORS):
+            self.interpreter.super().error(ErrorType.TYPE_ERROR, f"Got an int when expected another datatype")
+            return NIL_VAL
+        if type1 == InterpreterBase.BOOL_DEF and not (type1 in BINARY_BOOL_OPERATORS):
+            self.interpreter.super().error(ErrorType.TYPE_ERROR, f"Got a bool when expected another datatype")
+            return NIL_VAL
+        if type1 == InterpreterBase.STRING_DEF and not (type1 in BINARY_STRING_OPERATORS):
+            self.interpreter.super().error(ErrorType.TYPE_ERROR, f"Got a string when expected another datatype")
+            return NIL_VAL
+        match expression.elem_type:
+            case str(ADD):
+                return Element(type1, value=val1.get(VALUE) + val2.get(VALUE))
+            case str(SUBTRACT):
+                return Element(type1, value=val1.get(VALUE) - val2.get(VALUE))
+            case str(MULTIPLY):
+                return Element(type1, value=val1.get(VALUE) * val2.get(VALUE))
+            case str(DIVIDE):
+                return Element(type1, value=int(val1.get(VALUE) / val2.get(VALUE)))
+            case str(OR):
+                return Element(type1, value=(val1.get(VALUE) or val2.get(VALUE)))
+            case str(AND):
+                return Element(type1, value=(val1.get(VALUE) and val2.get(VALUE)))
+            case _:
+                self.interpreter.super().error(ErrorType.TYPE_ERROR, f"Binary operator {expression.elem_type} not implemented")
+                return NIL_VAL
+    
+    def evaluate_comparison_operation(self, expression: Element) -> Element:
+        val1 = self.evaluate_expression(expression.get(OPERAND_1))
+        val2 = self.evaluate_expression(expression.get(OPERAND_2))
+        type1 = val1.elem_type
+        type2 = val2.elem_type
+        if type1 != type2:
+            self.interpreter.super().error(ErrorType.TYPE_ERROR, f"Got {type1} and {type2} when expected same datatype")
+            return NIL_VAL
+        if type1 == InterpreterBase.INT_DEF and not (type1 in COMPARISON_INT_OPERATORS):
+            self.interpreter.super().error(ErrorType.TYPE_ERROR, f"Got an int when expected another datatype")
+            return NIL_VAL
+        if type1 == InterpreterBase.BOOL_DEF and not (type1 in COMPARISON_BOOL_OPERATORS):
+            self.interpreter.super().error(ErrorType.TYPE_ERROR, f"Got a bool when expected another datatype")
+            return NIL_VAL
+        if type1 == InterpreterBase.STRING_DEF and not (type1 in COMPARISON_STRING_OPERATORS):
+            self.interpreter.super().error(ErrorType.TYPE_ERROR, f"Got a string when expected another datatype")
+            return NIL_VAL
+        match expression.elem_type:
+            case str(EQUALS):
+                return Element(type1, value=(val1.get(VALUE) == val2.get(VALUE)))
+            case str(NOT_EQUALS):
+                return Element(type1, value=(val1.get(VALUE) != val2.get(VALUE)))
+            case str(LESS_THAN):
+                return Element(type1, value=(val1.get(VALUE) < val2.get(VALUE)))
+            case str(LESS_THAN_EQUALS):
+                return Element(type1, value=(val1.get(VALUE) <= val2.get(VALUE)))
+            case str(GREATER_THAN):
+                return Element(type1, value=(val1.get(VALUE) > val2.get(VALUE)))
+            case str(GREATER_THAN_EQUALS):
+                return Element(type1, value=(val1.get(VALUE) >= val2.get(VALUE)))
+            case _:
+                self.interpreter.super().error(ErrorType.TYPE_ERROR, f"Comparison operator {expression.elem_type} not implemented")
+                return NIL_VAL
+            
+
+    # Technically evaluates expressions, variables, and values, but variables and values are very little work so I fold them in
+    def evaluate_expression(self, expression: Element) -> Element:
+        expression_type = expression.elem_type
+        if expression_type == InterpreterBase.FCALL_DEF:
+            function_statement = Statement(self.interpreter, self, expression)
+            return function_statement.run()
+        if expression_type == InterpreterBase.VAR_DEF:
+            return self.get_var(expression.get(NAME))
+        if expression_type in VALUE_TYPES:
+            return expression
+        if expression_type in OPERATORS:
+            if expression_type in UNARY_OPERATORS:
+                return self.evaluate_unary_operation(expression)
+            if expression_type in BINARY_OPERATORS:
+                return self.evaluate_binary_operation(expression)
+            
+
+    
+    def run_assignment(self) -> None:
+        name = self.statement_node.get(NAME)
+        value = self.statement_node.get(VALUE)
+        self.set_var(name, self.evaluate_expression(value))
+
+    # Preloaded functions
+    def run_print(self) -> None:
+        args = self.statement_node.get(ARGS)
+        self.interpreter.super().output("".join([str(arg.get(VALUE)) for arg in args]))
+    
+    def get_input(self):
+        args = self.statement_node.get(ARGS)
+        if len(args) > 1:
+            self.interpreter.super().error(ErrorType.TYPE_ERROR, f"inputi takes at most 1 arguments, but got {len(args)}")
+            return
+        if len(args) == 1:
+            self.run_print()
+        input_val = self.interpreter.super().get_input()
+        return input_val
+
+    def run_inputi(self) -> Element:
+        input_val = self.get_input()
+        if not input_val or not input_val.isnumeric():
+            self.interpreter.super().error(ErrorType.TYPE_ERROR, f"inputi expected int, got {input_val}")
+            return NIL_VAL
+        return Element(InterpreterBase.INT_DEF, value=int(input_val))
+    
+    def run_inputs(self) -> Element:
+        input_val = self.get_input()
+        return Element(InterpreterBase.STRING_DEF, value=input_val)
+
+    def run(self) -> Element:
+        # Match statements in Python are stupid - if you match a var it will try and store it's value into it?
+        # I have no idea why it's like this, but it is, so by wrapping it in str() we get around this
+        # Also, if you access a var from another object it also works?  See evaluate_unary_operation for example
+        match self.type:
+            # Preloaded statements for built in functions (currently print, inputi, and inputs)
+            case str(PRINT):
+                self.run_print()
+            case str(INPUTI):
+                return self.run_inputi()
+            case str(INPUTS):
+                return self.run_inputs()
+            
+            # Statements that are not for preloaded functions
+            case str(ASSIGNMENT):
+                self.run_assignment()
+            case _:
+                self.interpreter.super().error(ErrorType.TYPE_ERROR, f"Statement type {self.type} not recognized")
+                return NIL_VAL
+        # By default, we just return nil
+        return NIL_VAL
